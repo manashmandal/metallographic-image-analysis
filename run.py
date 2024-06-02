fda1dcdea718708541f44b8c3a5a8d83cb640229
manashmandal <manashmndl@gmail.com>
2024-06-02 22:11:53 +0200

    fix

--- run.py
+++ run.py
@@ -1,12 +1,29 @@
-from preprocess import train_test_split, batch_generator_with_transform, collate_fn
+from preprocess import (
+    train_test_split,
+    batch_generator_with_transform,
+    collate_fn,
+    label_to_index_map,
+    index_to_label_map,
+    labels,
+)
 from datasets import Dataset
 from itertools import islice
+from transformers import ViTForImageClassification
+
+model_name_or_path = "google/vit-base-patch16-224-in21k"
+
 
 if __name__ in ("__main__", "run"):
     train_test = train_test_split()
 
-    train_generator = batch_generator_with_transform(data=train_test.train_data)
-    sliced = islice(train_generator, 0, 5)
-    collated = collate_fn(list(sliced))
+    train_generator = batch_generator_with_transform(data=train_test.train_data[:1])
+    collated = collate_fn(list(train_generator))
 
-    print(collated)
+    model = ViTForImageClassification.from_pretrained(
+        model_name_or_path,
+        num_labels=len(labels),
+        id2label=index_to_label_map,
+        label2id=label_to_index_map,
+    )
+
+    print(model(collated["pixel_values"]), return_tensors="pt")
